175955a1a80ce5a1fd95bc798cbbea2d6844918b
taro <taro@example.com>
2023-09-21 13:30:18 +0000

    kadai04

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,17 @@ use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
     Route::get('news/create', 'add');
 });
+
+
+
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+    Route::get('profile/create', 'add');
+    Route::get('profile/edit', 'edit');
+});
+
+// ＜課題０４－３＞
+// Route::controller(AAAController::class)->group(function() {
+    // Route::get('xxx', 'bbb');
+   
+// });
