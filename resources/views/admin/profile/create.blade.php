9a1b9005b7b7babb9b8e0a7dced608cb3d4e20ee
taro <taro@example.com>
2023-10-09 05:30:22 +0000

    kadai08

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -11,6 +11,50 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>My　プロフィール</h2>
+                 <form action="{{ route('admin.profile.create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        </div>
+                    </div>
+                     <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                            <div class="form-check form-check-inline">
+                                <input type="radio" class="form-check-input col-md-5" name="inlineRadioOptions" value="option1">
+                                <label class="form-check-label" for="inlineRadio1">男性</label> 
+                            </div>
+                             <div class="form-check form-check-inline">
+                                <input type="radio" class="form-check-input col-md-5" name="inlineRadioOptions" value="option2">
+                                <label class="form-check-label" for="inlineRadio2">女性</label> 
+                             </div>
+                         </div>
+                    </div>
+                     <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="5">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    
+                    @csrf
+                    <input type="submit" class="btn btn-primary" value="送信">
+                </form>
             </div>
         </div>
     </div>
